620f58ed35aa6c213a5aaa8415cae40161a21c8f
Charilaos Karametos <charis.karametos@gmail.com>
2026-06-12 20:27:26 +0300

    fix pursuits for the last time

--- src/main/kotlin/org/breakthebot/breakthelibrary/models/Pursuits.kt
+++ src/main/kotlin/org/breakthebot/breakthelibrary/models/Pursuits.kt
@@ -14,31 +14,30 @@
  * You should have received a copy of the GNU General Public License
  * along with BreakTheLibrary. If not, see <https://www.gnu.org/licenses/>.
  */
-/*
- * This file is part of BreakTheLibrary.
- *
- * BreakTheLibrary is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * BreakTheLibrary is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with BreakTheLibrary. If not, see <https://www.gnu.org/licenses/>.
- */
 package org.breakthebot.breakthelibrary.models
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.breakthebot.breakthelibrary.utils.SerializableUUID
 
+
+sealed class PursuitTop
+
 @Serializable
-data class PursuitTop(
-    val uuid: SerializableUUID? = null,
+data class PlayerPursuitTop(
+    val player: SerializableUUID? = null,
+    val score: Float = 0F
+)
+
+@Serializable
+data class TownPursuitTop(
+    val town: SerializableUUID? = null,
+    val score: Float = 0F
+)
+
+@Serializable
+data class NationPursuitTop(
+    val town: SerializableUUID? = null,
     val score: Float = 0F
 )
 
@@ -46,21 +45,21 @@ data class PursuitTop(
 data class PlayerPursuits(
     val name: String,
     val isActive: Boolean,
-    val top: Map<String, PursuitTop>?
+    val top: Map<String, PlayerPursuitTop>?
 )
 
 @Serializable
 data class TownPursuits(
     val name: String,
     val isActive: Boolean,
-    val top: Map<String, PursuitTop>?
+    val top: Map<String, TownPursuitTop>?
 )
 
 @Serializable
 data class NationPursuits(
     val name: String,
     val isActive: Boolean,
-    val top: Map<String, PursuitTop>?
+    val top: Map<String, NationPursuitTop>?
 )
 
 @Serializable
